8be399ddb2964a3638876a195b992607df1bda6b
Brian Hines <brian@projectweekend.net>
2014-06-20 21:23:37 -0500

    For real this time

--- starbug/main.py
+++ starbug/main.py
@@ -6,12 +6,12 @@ import click
 @click.command()
 @click.option('--type', default='usb', help='The serial connection type: usb or gpio')
 @click.option('--rate', default=9600, help='The serial data rate (defaults to 9600)')
-def main(connection_type, data_rate):
-    if connection_type == 'pin':
+def main(type, rate):
+    if type == 'pin':
         # make this work
         pass
     else:
-        SERIAL = serial.Serial('/dev/ttyUSB0', 9600)
+        SERIAL = serial.Serial('/dev/ttyUSB0', rate)
     # Give serial port a little time to setup
     time.sleep(2)
     while True:
