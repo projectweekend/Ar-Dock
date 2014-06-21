7b5fe77e527b64920c93eae334046a674f7650a9
Brian Hines <brian@projectweekend.net>
2014-06-21 07:01:53 -0500

    Simplify

--- utils/send_data.py
+++ utils/send_data.py
@@ -8,19 +8,17 @@ from datetime import datetime
 SEND_URL = os.getenv('SEND_URL', 'http://holly.local/api/starbug/sensor')
 
 
-def log_failure(status_code, response_body):
+def log_post_failure(status_code, response_body):
     entry_template = '{0}: {1} - "{2}"'
     log_time = datetime.now()
     logging.info(entry_template.format(log_time, status_code, response_body))
 
 
 def json_post(url, data):
+
     headers = {'content-type': 'application/json'}
     data = json.dumps(data)
-    return requests.post(SEND_URL, data=data, headers=headers)
 
-
-def send_starbug_data(data):
-    response = json_post(SEND_URL, data)
+    response = requests.post(url, data=data, headers=headers)
     if response.status_code != requests.codes.created:
-        log_failure(response.status_code, response.json())
+        log_post_failure(response.status_code, response.json())
