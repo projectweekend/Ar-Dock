1157a86c98f73c94418975552529ec253ff3b493
Brian Hines <brian@projectweekend.net>
2014-06-20 21:08:12 -0500

    Little things

--- starbug/test.py
+++ starbug/test.py
@@ -1,12 +1,19 @@
 import time
 import serial
+import click
 
 
-SERIAL = serial.Serial('/dev/tty.usbserial-A5027VO5', 9600)
-time.sleep(5)
-
-
-def main():
+@click.command()
+@click.option('--type', default='usb', help='The serial connection type: usb or gpio')
+@click.option('--rate', default=9600, help='The serial data rate (defaults to 9600)')
+def main(connection_type, data_rate):
+    if connection_type == 'pin':
+        # make this work
+        pass
+    else:
+        SERIAL = serial.Serial('/dev/ttyUSB0', 9600)
+    # Give serial port a little time to setup
+    time.sleep(2)
     while True:
         data = SERIAL.readline()
         print(data)
