59740199e2e49a5f2398377861da7cc656aa9ac0
Brian Hines <brian@projectweekend.net>
2014-06-20 22:38:03 -0500

    Add some utils

--- utils/send_data.py
+++ utils/send_data.py
@@ -1,4 +1,26 @@
 import os
+import json
 import requests
+import logging
+from datetime import datetime
 
-SEND_URL = os.getenv('AR_DOCK_TRANSMIT_URL', '')
+
+SEND_URL = os.getenv('SEND_URL', 'http://holly.local/api/starbug/sensor')
+
+
+def log_failure(status_code, response_body):
+    entry_template = '{0}: {1} - "{2}"'
+    log_time = datetime.now()
+    logging.info(entry_template.format(log_time, status_code, response_body))
+
+
+def json_post(url, data):
+    headers = {'content-type': 'application/json'}
+    data = json.dumps(data)
+    return requests.post(SEND_URL, data=data, headers=headers)
+
+
+def send_starbug_data(data):
+    response = json_post(SEND_URL, data)
+    if response.status_code != requests.codes.created:
+        log_failure(response.status_code, response.json())
