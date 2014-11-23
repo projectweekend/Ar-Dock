54f633747cfacbb07583cbcf00536ac33b9d2848
Brian Hines <brian@projectweekend.net>
2014-11-23 15:01:59 -0600

    Add node sensor service

---
+++ services/environmental_sensors.js
@@ -0,0 +1,46 @@
+var jackrabbit = require( "jackrabbit" );
+var serialport = require( "serialport" );
+
+
+var serialAddress = process.env.SERIAL_ADDRESS;
+var serialRate = process.env.SERIAL_RATE;
+var serialMessage = process.env.SERIAL_MESSAGE;
+var rabbitURL = process.env.RABBIT_URL;
+
+
+var broker = jackrabbit( RABBIT_URL, 1 );
+var serialPort = new serialport.SerialPort( serialAddress, {
+    baudrate: serialRate,
+    parser: serialport.parsers.readline( "\n" )
+} );
+
+var parseSensorData = function ( sensorData ) {
+    var output = {};
+    sensorData.split( "|" ).map( function ( sensorReading ) {
+        var parts = sensorReading.split( ":" );
+        output[ parts[ 0 ] ] = parseFloat( parts[ 1 ] );
+    } );
+    return output;
+};
+
+var brokerOnReady = function () {
+    broker.handle( "sensor.get", function ( message, ack ) {
+        serialPort.on( "data", function ( data ) {
+            ack( data );
+        } );
+        serialPort.write( "A", function ( err, data ) {
+            if ( err ) {
+                console.log( "Error writing to serial port: " + err );
+            }
+        } );
+    } );
+};
+
+var brokerOnConnect = function () {
+    broker.create( "sensor.get", { prefetch: 5 }, brokerOnReady );
+};
+
+// Kick it off when serial port is open
+serialPort.on( "open", function () {
+    broker.on( "connected", brokerOnConnect );
+} );
